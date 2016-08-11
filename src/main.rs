2c045bccef837d98a942cd9755b59a5cbb4539cd
Kerosene2000 <renault.cle@gmail.com>
2016-08-12 01:34:42 +0200

    error are catched

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
 #[macro_use] extern crate nom;
+#[macro_use] extern crate colorify;
 
 mod surface_points;
 
+use std::env;
+use std::fs::File;
+use std::io::Read;
 use surface_points::SurfacePoints;
 
 fn main() {
-    let test = b"   (10 ,11 ,12) (1,5,9)(2,3,5)";
-    let surface_points = SurfacePoints::from_buffer(test);
-    println!("{:?}", surface_points);
+    let mut surfaces_points = Vec::new();
+    let args = env::args().skip(1);
+    for arg in args {
+        match File::open(arg.clone()) {
+            Ok(mut file) => {
+                let mut vec = Vec::new();
+                file.read_to_end(&mut vec);
+                match SurfacePoints::from_buffer(&vec) {
+                    Ok(surface_points) => surfaces_points.push(surface_points),
+                    Err(err) => printlnc!(red: "{}: Syntax error", arg)
+                }
+            },
+            Err(err) => printlnc!(red: "{}: {}", arg, err)
+        }
+    }
+    println!("{:?}", surfaces_points);
 }
