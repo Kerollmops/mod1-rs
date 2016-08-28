c6eab0f984052b28d5a822d02f70a7b5a7c1c930
Kerosene2000 <renault.cle@gmail.com>
2016-08-28 16:23:08 +0200

    fixed surfacePoint I think

--- src/height_map.rs
+++ src/height_map.rs
@@ -1,5 +1,5 @@
 use ndarray::{Array, Ix};
-use ::surface_points::SurfacePoints;
+use ::surface_points::{SurfacePoints, SurfacePoint};
 
 #[derive(Debug)]
 pub struct HeightMap(Array<f32, (Ix, Ix)>);
@@ -17,25 +17,21 @@ fn distance((a_x, a_y): (f32, f32), (b_x, b_y): (f32, f32)) -> f32 {
 }
 
 fn inverse_distance_weighting(points: &SurfacePoints, array: &mut Array<f32, (Ix, Ix)>) {
-    let array_rows = array.cols() as f32;
-    let array_cols = array.rows() as f32;
-    for (idx, height) in array.indexed_iter_mut() {
-        let idx = (idx.0 as f32, idx.1 as f32);
-        let (numerator, mut denominator) = points.iter().fold((0.0, 0.0), |acc, &p| {
-            let dist = distance((p.x as f32, p.y as f32), idx);
+    let (rows, cols) = (array.rows() as f32, array.cols() as f32);
+    for (pos, height) in array.indexed_iter_mut() {
+        let pos = (pos.0 as f32, pos.1 as f32);
+        let (numerator, mut denominator) = points.iter().fold((0.0, 0.0), |acc, &point| {
+            let SurfacePoint{ x, y, z } = point;
+            let dist = distance((x as f32, y as f32), pos);
             let weight = weight(dist);
-            println!("({:?}), weight: {}", p.z, weight);
-            let add = if dist != 0.0 { weight * p.z as f32 } else { p.z as f32 };
+            let add = if dist != 0.0 { weight * z as f32 } else { z as f32 };
             (acc.0 + add, acc.1 + weight)
         });
-        // FIXME uncomment this
-        // denominator += weight(idx.0);                            // dist (x, 0)
-        // denominator += weight(idx.1);                            // dist (0, y)
-        // denominator += weight((array_rows - 1.0 - idx.0).abs()); // dist (x_max, 0)
-        // denominator += weight((array_cols - 1.0 - idx.1).abs()); // dist (0, y_max)
+        denominator += weight(pos.0);              // dist (x, 0)
+        denominator += weight(pos.1);              // dist (0, y)
+        denominator += weight(rows - 1.0 - pos.0); // dist (x_max, 0)
+        denominator += weight(cols - 1.0 - pos.1); // dist (0, y_max)
         *height = numerator / denominator;
-        println!("{:?}/{:?}, height: {:?}", numerator, denominator, *height);
-        println!("-------------");
     }
 }
 
